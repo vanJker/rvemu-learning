29fb3538a27d615a7fbdeb1f0f02a2b0dfc03771
vanJker <1847722163@qq.com>
2023-04-16 16:23:18 +0800

    [Lesson-3] Read elf segments

--- src/rvemu.c
+++ src/rvemu.c
@@ -8,6 +8,8 @@ int main(int argc, char *argv[]) {
     machine_t machine = {0};
     machine_load_program(&machine, argv[1]);
 
+    printf("hello, rvemu\n");
     printf("entry: 0x%lx\n", machine.mmu.entry);
+    printf("host alloc: 0x%lx\n", machine.mmu.host_alloc);
     return 0;
 }
